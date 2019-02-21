22115b7acb392d73aa2a3e394349eea94834a6ca
Tryston Perry <TrystonMPerry@gmail.com>
2019-02-21 13:41:54 -0800

    Redesign of project section and new animations

--- public/scripts/nav-menu.js
+++ public/scripts/nav-menu.js
@@ -3,16 +3,31 @@ const menu = document.getElementById('menu');
 
 let isMenuVisible = false;
 
-menuBtn.addEventListener('click', () => toggleMenu())
-
-function toggleMenu() {
+function toggleMenu(e) {
   if(isMenuVisible) {
-    isMenuVisible = false;
-    menu.style.display = 'none';
+    menu.classList.add('dragDownReverse-play');
     e.target.style.color = 'white';
+    setTimeout(() => {
+      isMenuVisible = false;
+      menu.style.display = 'none';
+      menu.classList.remove('dragDownReverse-play');
+    },240)
+   
   } else {
     isMenuVisible = true;
     menu.style.display = 'block';
-    e.target.style.color = '#0692FF';
+    menu.classList.add('dragDown-play');
+    setTimeout(() => {
+      e.target.style.color = '#0692FF';
+      menu.classList.remove('dragDown-play');
+    }, 250);
   }
+}
+
+function viewMore(e) {
+  e.preventDefault();
+  e.target.classList.add('fadeOutText-play');
+  const div = e.target.parentNode.getElementsByClassName('hidden')[0];
+  div.style.display = 'block';
+  div.classList.add('fadeInText-play');
 }
